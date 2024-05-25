d87600574cf795f2419716dd010078a03d23bb6f
ocarolinne <ocarolinne@gmail.com>
2024-05-25 16:54:45 -0700

    Upload first version of the project

---
+++ PyBank/main.py
@@ -0,0 +1,74 @@
+# Import dependencies
+import os
+import csv
+
+read_dir = 'PyBank/Resources/'
+filename = 'budget_data.csv'
+
+csv_path = os.path.join(read_dir, filename)
+
+# Open File
+
+with open(csv_path) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    csv_header = next(csv_file)
+    
+ # Declare variables and lists   
+    changes_values = []
+    profit = []  
+    previous_value = 0
+    budget_data = []
+
+ # Read the file and create a numeric index to calculate differences between months  
+    for index, line in enumerate(csv_reader):
+       # Total months included in dataset
+        budget_data.append(line)   
+        
+        # The profit information to calculate the total profit
+        profit.append(int(line[1]))  
+        
+        # CCalculate changes in profit/loss over the entire period
+        if index == 0: 
+            previous_value = int(line[1]) 
+        
+        else:   
+            changes_values.append(int(line[1])-previous_value)       
+            previous_value = int(line[1])
+
+ # # Calculate the sum of profits, the maximum and minimum value and the average profit variation                             
+    total_profit = sum(profit)    
+    max_change = max(changes_values)
+    min_change = min(changes_values)
+    mean_change = (sum(changes_values) / len(changes_values))   
+    
+    index_max_change = changes_values.index(max(changes_values))
+    index_max_loss = changes_values.index(min(changes_values))
+    data_max_change = budget_data[index_max_change][0]
+    data_min_change = budget_data[index_max_loss][0]
+ 
+    
+# Print the analysis to the terminal:
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(budget_data)}")
+print(f"Total: ${total_profit}")
+print(f"Average Change: ${mean_change:.2f}")
+print(f"Greatest Increase in Profits: {data_max_change} (${max_change})")
+print(f"Greatest Decrease in Profits: {data_min_change} (${min_change})")   
+
+#Export result to text file
+
+txt_directory = "PyBank/analysis"
+txt_path = os.path.join(txt_directory, "budget_analysis.txt")
+os.makedirs(txt_directory, exist_ok=True)
+
+with open(txt_path, "w") as outfile:
+    
+    outfile.write("Financial Analysis\n")
+    outfile.write("----------------------------\n")
+    outfile.write(f"Total Months: {len(budget_data)}\n")
+    outfile.write(f"Total: ${total_profit}\n")
+    outfile.write(f"Average Change: ${mean_change:.2f}\n")
+    outfile.write(f"Greatest Increase in Profits: {data_max_change} (${max_change})\n")
+    outfile.write(f"Greatest Decrease in Profits: {data_min_change} (${min_change})\n")  
---
+++ PyPoll/main.py
@@ -0,0 +1,79 @@
+# Import dependencies
+import os
+import csv
+
+read_dir = 'PyPoll/Resources/'
+filename = 'election_data.csv'
+
+csv_path = os.path.join(read_dir, filename)
+
+# Declare variables and lists  
+
+total_votes = 0
+unique_candidate = []
+current_candidate = ()
+candidates_list = []
+votes_candidates = {}
+winner = ()
+
+# Open File
+ 
+with open(csv_path) as csv_file:
+    #csv_reader = csv.DictReader(csv_file)   #// Read de file and interacted with the header
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    csv_header = next(csv_file)
+  
+ # Read the file, sum the total votes and create a list of candidates 
+    for line in csv_reader:
+        total_votes = total_votes + 1
+        
+        candidates_list.append(line[2])    
+        
+        current_candidate = (line[2])    
+    
+    # Use the candidate list to create a single data  dictionary containing candidate names and vote totals
+        if current_candidate not in unique_candidate:      
+            unique_candidate.append(current_candidate)
+            votes_candidates[current_candidate] = 1    
+        else:
+            votes_candidates[current_candidate] += 1  
+    
+    # Calculate the percentage of votes        
+    for candidate, votes in votes_candidates.items():
+        percentage_votes = (votes / total_votes) * 100
+    
+        winner = max(votes_candidates, key=votes_candidates.get)
+        
+# Print the analysis to the terminal:
+        
+print("Election Results")   
+print("-------------------------")        
+print(f'Total Votes: {total_votes}')
+print("-------------------------") 
+for candidate, votes in votes_candidates.items():
+        percentage_votes = (votes / total_votes) * 100
+        winner = max(votes_candidates, key=votes_candidates.get)
+        print(f"{candidate}: {percentage_votes:.3f}% % ({votes})")
+print("-------------------------") 
+print(f'Winner: {winner}')
+print("-------------------------")  
+
+#Export result to text file
+
+txt_directory = "PyPoll/analysis"
+txt_path = os.path.join(txt_directory, "election_analysis.txt")
+os.makedirs(txt_directory, exist_ok=True)   
+
+with open(txt_path, "w") as outfile:
+    
+    outfile.write("Election Results\n")
+    outfile.write("----------------------------\n")
+    outfile.write(f"Total Votes: {total_votes}\n")
+    outfile.write("----------------------------\n")
+    for candidate, votes in votes_candidates.items():
+        percentage_votes = (votes / total_votes) * 100
+        winner = max(votes_candidates, key=votes_candidates.get)
+        outfile.write(f"{candidate}: {percentage_votes:.3f}% % ({votes})\n")
+    outfile.write("----------------------------\n")
+    outfile.write(f"Winner: {winner}\n")
+    outfile.write("----------------------------\n") 
